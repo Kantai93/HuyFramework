ef7911c855d7269112ace686b4acc807f1a1394e
Kantai <>
2019-07-22 16:02:28 +0700

    Prject setup

---
+++ HuyFrameWork/HuyFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  HuyFrameWork.h
+//  HuyFrameWork
+//
+//  Created by YenPhan on 7/22/19.
+//  Copyright © 2019 YenPhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HuyFrameWork.
+FOUNDATION_EXPORT double HuyFrameWorkVersionNumber;
+
+//! Project version string for HuyFrameWork.
+FOUNDATION_EXPORT const unsigned char HuyFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HuyFrameWork/PublicHeader.h>
+
+
